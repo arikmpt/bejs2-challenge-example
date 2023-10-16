fcd19af38aac6b91cc8134429eaf42499442deae
Ari <ariteknologi@gmail.com>
2023-10-16 20:02:18 +0700

    add authenticate

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,4 +1,12 @@
 const { PrismaClient } = require('@prisma/client')
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const cryptPassword = async (password) => {
+    const salt = await bcrypt.genSalt(5);
+
+    return bcrypt.hash(password, salt)
+}
 
 const prisma = new PrismaClient();
 
@@ -8,7 +16,7 @@ module.exports = {
             data: {
                 name: req.body.name,
                 email: req.body.email,
-                password: req.body.password,
+                password: await cryptPassword(req.body.password),
                 profile: {
                     create: {
                         identity_number: req.body.identity_number,
@@ -19,8 +27,47 @@ module.exports = {
             }
         });
 
-
         return res.json({
+            data: user
+        })
+    },
+
+    loginUser: async(req, res) => {
+        const findUser = await prisma.users.findFirst({
+            where: {
+                email: req.body.email
+            }
+        })
+
+        if(!findUser) {
+            return res.status(404).json({
+                error: 'User not exists'
+            });
+        }
+
+        if(bcrypt.compareSync(req.body.password, findUser.password)) {
+            const token = jwt.sign({ id: findUser.id} , 'secret_key', { expiresIn : '6h'})
+
+            return res.status(200).json({
+                data: {
+                    token
+                }
+            })
+        }
+
+        return res.status(403).json({
+            error: 'Invalid credentials'
+        })
+    },
+
+    getProfile: async(req, res) => {
+        const user = await prisma.users.findUnique({
+            where: {
+                id: res.user.id
+            }
+        })
+
+        return res.status(200).json({
             data: user
         })
     }
--- routers.js
+++ routers.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const userController = require('./controllers/userControllers')
+const checkToken = require('./middleware/checkToken')
 
 router.get('/', (req, res) => {
     return res.json({
@@ -8,6 +9,8 @@ router.get('/', (req, res) => {
     })
 })
 
-router.post('/users', userController.registerUser)
+router.post('/auth/register', userController.registerUser)
+router.post('/auth/login', userController.loginUser)
+router.get('/auth/authenticate', checkToken, userController.getProfile)
 
 module.exports = router
